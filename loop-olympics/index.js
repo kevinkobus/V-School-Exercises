ea0e4bbdde4cc5af6a6aad75dc4f0a976d4b809c
Kevin Kobus <kevinkobus@gmail.com>
2022-08-28 14:55:10 -0600

    more code completed for the exercise

--- loop-olympics/index.js
+++ loop-olympics/index.js
@@ -27,6 +27,7 @@
 // 2. Write a for loop that prints to the console only even numbers 0 through 100.
 // 3. Write a for loop that will push every other fruit to an array.`const fruit = ["banana", "orange", "apple", "kiwi", "pear", "peach"]`
 
+// 1.
 // needs work - doesn't console as an array
 
 // var arr = []
@@ -37,14 +38,14 @@
 //   console.log(resultArr) 
 // }
 
-
+// 2.
 // for(var i = 0; i <= 100; i++) {
 //     if(i % 2 === 0) {
 //         console.log(i)
 //     }
 // }
 
-
+// 3.
 // const fruit = ["banana", "orange", "apple", "kiwi", "pear", "peach"]
 
 // for (var i = 0; i < fruit.length; i++) {
@@ -53,7 +54,13 @@
 //     }
 // }
 
+// # **Silver Medal**
 
+// 1. Write a loop that will print out all the names of the people of the people array
+// 2. Write a loop that pushes the names into a `names` array, and the occupations into an `occupations` array.
+// 3. Write a loop that pushes every other name to an array starting with the first person, in this case "Harrison Ford", and every other occupation to *another* array starting with, in this case, "Singer".
+
+// Think about what good names for the arrays would be. Since it will be an array of each name or occupation, the plurals of name and occupation would be appropriate.
 
 const peopleArray = [
     {
@@ -74,6 +81,50 @@ const peopleArray = [
     }
   ]
   
-  // ["Harrison Ford", "Joe Biden"] // names
-  // ["Singer", "Entertainer"] // occupations
+//   1.
+// for(var i = 0; i < peopleArray.length; i++)
+//   console.log(peopleArray[i].name)
+
+// 2.
+
+
+
+// ["Harrison Ford", "Joe Biden"] // names
+// ["Singer", "Entertainer"] // occupations
+
   
+ 
+  
+
+
+
+//   # **Gold Medal - Nesting**
+
+// 1. Create an array that mimics a grid like the following using nested `for` loops:
+
+// ```
+// [[0, 0, 0],
+// [0, 0, 0],
+// [0, 0, 0]]
+
+// ```
+
+// 2.Create an array that mimics a grid like the following using nested `for` loops:
+
+// ```
+// [[0, 0, 0],
+// [1, 1, 1],
+// [2, 2, 2]]
+
+// ```
+
+// 3.Create an array that mimics a grid like the following using nested `for` loops:
+
+// ```
+// [[0, 1, 2],
+// [0, 1, 2],
+// [0, 1, 2]]
+
+// ```
+
+// 1. Given a grid like the previous ones, write a nested `for` loop that would change every number to an x.
