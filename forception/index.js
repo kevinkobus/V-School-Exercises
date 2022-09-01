82270007c6a439afe1ba250b5247db24ee695773
Kevin Kobus <kevinkobus@gmail.com>
2022-09-01 15:06:58 -0600

    started forception exercise

---
+++ forception/index.js
@@ -0,0 +1,14 @@
+var people = ["Jon", "Jacob", "Jingle", "Heimer", "Schmidt"]
+var alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+var upperCaseAlpha = alphabet.toUpperCase()
+var alphabetArr = upperCaseAlpha.split("")
+// console.log(alphabetArr)
+
+
+
+for(var i = 1; i < people; i++)
+var addAlpha = people.splice([i], 0, alphabetArr)
+
+
+console.log(addAlpha)
