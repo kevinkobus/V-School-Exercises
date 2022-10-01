b38f5c4c91fef64f515f0922d19805d1ae825703
Kevin Kobus <kevinkobus@gmail.com>
2022-10-01 10:43:16 -0600

    updated fizzbuzz exercise

--- fizzbuzz/app.js
+++ fizzbuzz/app.js
@@ -1,15 +1,22 @@
-for (var i = 1; i <= 100; i++) {
-
-    if (i % 3 === 0 && i % 5 === 0) {
-        console.log("fizzbuzz")
-    }
-    else if (i % 3 === 0) {
-        console.log("fizz")
-    }
-    else if (i % 5 === 0) {
-        console.log("buzz")
-    }
-    else {
-        console.log(i)
-    }
-}
+// function fizzBuzz(numArr) {
+  
+    for (var i = 1; i <= 100; i++) {
+        var nums = []
+        var numArr = nums.push(i)
+        
+          if (i % 3 === 0 && i % 5 === 0) {
+              console.log("fizzbuzz")
+          }
+          else if (i % 3 === 0) {
+              console.log("fizz")
+          }
+          else if (i % 5 === 0) {
+              console.log("buzz")
+          }
+          else {
+              console.log(i)
+          }
+      }
+      // }
+      
+      // console.log(fizzBuzz(numArr))
