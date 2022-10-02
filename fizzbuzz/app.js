d9b71163605898a0505d2df070ec7cb61887fc8d
Kevin Kobus <kevinkobus@gmail.com>
2022-10-02 14:25:38 -0600

    improved some code

--- fizzbuzz/app.js
+++ fizzbuzz/app.js
@@ -1,22 +1,37 @@
-// function fizzBuzz(numArr) {
+for (var i = 1; i <= 100; i++) {
   
-    for (var i = 1; i <= 100; i++) {
-        var nums = []
-        var numArr = nums.push(i)
-        
-          if (i % 3 === 0 && i % 5 === 0) {
-              console.log("fizzbuzz")
-          }
-          else if (i % 3 === 0) {
-              console.log("fizz")
-          }
-          else if (i % 5 === 0) {
-              console.log("buzz")
-          }
-          else {
-              console.log(i)
-          }
-      }
-      // }
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("fizzbuzz")
+    }
+    else if (i % 3 === 0) {
+        console.log("fizz")
+    }
+    else if (i % 5 === 0) {
+        console.log("buzz")
+    }
+    else {
+        console.log(i)
+    }
+}  
+    const nums = []
+    const numArr = nums.push(i)
+    
+    // console.log(nums)
+
+    let fizzCount = 0
+    let buzzCount = 0     
+    let fizzBuzzCount = 0
       
-      // console.log(fizzBuzz(numArr))
+ nums.forEach(element => {
+   if (element === "fizz") {
+     fizzCount =+ 1
+   }
+     if (element === "buzz") {
+     buzzCount =+ 1
+   }  
+     if (element === "fizzbuzz") {
+     fizzBuzzCount =+ 1 
+   }
+ }) 
+
+ console.log("fizz: " + fizzCount, "buzz: " + buzzCount, "fizzbuzz: " + fizzBuzzCount)
--- problem-solving-practice/app.js
+++ problem-solving-practice/app.js
@@ -1,9 +1,16 @@
-var numArr = ["21", "76", "52", "39", "26", "43", "61", "87", "94"]
-var largestNum = 0
-
-for(var i = 0; i < numArr.length; i++) {
-  if(numArr[i] >= largestNum) {
-    largestNum = numArr[i]
+function largest(numArr) {
+  let largestNum = 0
+  
+  for(var i = 0; i < numArr.length; i++) {
+    if(numArr[i] > largestNum) 
+      largestNum = numArr[i]
+    }
+    return largestNum
   }
-}
-console.log("The largest number is " + largestNum)
+  
+  console.log(largest(["6", "13", "250", "3"]))
+  
+  console.log(largest(["3", "5", "2", "8", "1"]))
+  
+  console.log(largest(["-13", "40", "3", "0", "19", "22"]))
+
