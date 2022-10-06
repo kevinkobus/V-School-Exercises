39bdf6d43848941cd61032236c65c47218796ed8
Kevin Kobus <kevinkobus@gmail.com>
2022-10-06 12:31:53 -0600

    fixed code for node calculator exercise

--- node-calculator.js
+++ node-calculator.js
@@ -1,11 +1,7 @@
 const readlineSync = require("readline-sync")
 
-var num1 = readlineSync.question("Please enter your first number", {
-    
-})
-var num2 = readlineSync.question("Please enter your second number", {
-    
-})
+var num1 = readlineSync.question("Please enter your first number")
+var num2 = readlineSync.question("Please enter your second number")
 
 console.log("So your two numbers are " + num1 + " and " + num2)
 
