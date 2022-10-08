87d6240174e8d29575f544796c3a2a5c93385911
Kevin Kobus <kevinkobus@gmail.com>
2022-10-08 12:09:19 -0600

    tweaks to escape room but still doesn't work quite right

--- escape-room/index.js
+++ escape-room/index.js
@@ -1,17 +1,17 @@
 const readline = require("readline-sync")
 
-//Intro to situation
+//Intro to room
 const name = readline.question("What is your name? ")
 console.log("Hi " + name + "! Your in a room and you want to get out")
 let go = readline.question("Are you ready? Type Y for yes, or N for no ")
 
-function start() {
+function start(go) {
     if (go === "y" || go === "Y") {
         console.log("Lets Go!")
     }
     else if (go === "n" || go === "N") {
         console.log("Ok, but don't forget you're still in the room")
-        start()
+        start(go)
     }
 }
 
@@ -20,39 +20,40 @@ let choice = readline.question("Here are your options: A. Stick your hand in the
 
 let count = 0
 
-const findKey = (function () {
+const findKey = (function (choice) {
     if (choice === "A" || choice === "a") {
         count++
         console.log("Bad choice! You've been sucked into a black hole. Try again.")
-
         choice = readline.question("Here are your options again: A. Stick your hand in the hole. B. Find the key. C. Open the door.")
-        findKey()
+        findKey(choice)
     }
+
     else if (choice === "B" || choice === "b") {
         count++
         console.log("Nice, you've found the key!")
         escapeRoom()
     }
+
     else if (choice === "C" || choice === "c") {
         count++
         console.log("Bummer, it's locked! Try again")
-
         choice = readline.question("Here are your options again: A. Stick your hand in the hole. B. Find the key. C. Open the door. ")
-        findKey()
+        findKey(choice)
     }
 
     else {
         count++
         console.log("Umm, not sure what that means, try again")
-
         choice = readline.question("Here are your options again: A. Stick your hand in the hole. B. Find the key. C. Open the door. ")
-        findKey()
+        findKey(choice)
     }
 
 })
-//Room loop after key
-const escapeRoom = function () {
-    let secondChoice = readline.question("Here are your remaining options: A. Stick your hand in the hole. B. Open the door. ")
+
+//Room loop after key is found
+let secondChoice = readline.question("Here are your remaining options: A. Stick your hand in the hole. B. Open the door. ")
+
+const escapeRoom = (function () {    
     if (secondChoice === "A" || secondChoice === "a") {
         count++
         console.log("Bad choice! You've been sucked into a black hole. Try again.")
@@ -71,5 +72,5 @@ const escapeRoom = function () {
         escapeRoom()
     }
 
-}
+})
 
