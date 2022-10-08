d281b2d5a0fe0360f9879d93170536ffd254affb
Kevin Kobus <kevinkobus@gmail.com>
2022-10-08 15:44:42 -0600

    worked on forception exercise

--- forception/index.js
+++ forception/index.js
@@ -1,11 +1,20 @@
 var people = ["Jon", "Jacob", "Jingle", "Heimer", "Schmidt"]
 var alphabet = "abcdefghijklmnopqrstuvwxyz"
 
-var upperCaseAlpha = alphabet.toUpperCase()
-var alphabetArr = upperCaseAlpha.split("")
-// console.log(alphabetArr)
+function forception(people, alphabet) {
+    for (var i = 0; i < people.length; i++) {
+        people[i] = people[i] + ":"     
+    }
+    alphabetArr = alphabet.split("")
+    // console.log(alphabetArr)
 
-
-
-for(var i = 1; i < people; i++)
-var addAlpha = people.splice([i], 0, alphabetArr)
+    var combArr = []
+    for (var i = 0; i < people.length; i++) {
+      combArr.push(people[i])
+      for (var j = 0; j < alphabetArr; j++) {
+        combArr.push(alphabettArr[j])
+      }
+    }
+      return combArr
+}
+console.log(forception(people, alphabet))
